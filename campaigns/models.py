fb97a7fd10b42cf301a1bba86bb6d4c4ed3d1db8
Robin Schroer <sulami@peerwire.org>
2015-01-17 02:05:09 +0100

    add basic models

--- campaigns/models.py
+++ campaigns/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+AUTH_USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
+
+class Campaign(models.Model):
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(AUTH_USER_MODEL)
+
+class Session(models.Model):
+    campaign = models.ForeignKey(Campaign)
+    number = models.IntegerField()
+
+class Thread(models.Model):
+    campaign = models.ForeignKey(Campaign)
+    name = models.CharField(max_length=100)
+
+class Event(models.Model):
+    campaign = models.ForeignKey(Campaign)
+    session = models.ForeignKey(Session)
+    threads = models.ManyToManyField(Thread)
+    content = models.TextField()
+
