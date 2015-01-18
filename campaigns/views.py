dc364a1810ee2d538801472dcfaf34b4c75aa446
Robin Schroer <sulami@peerwire.org>
2015-01-18 21:16:15 +0100

    enable selection of threads and sessions

--- campaigns/views.py
+++ campaigns/views.py
@@ -9,11 +9,23 @@ def campaign(request, cid):
     threads = campaign.thread_set.all()
     sessions = campaign.session_set.all()
     events = campaign.event_set.all()
+
     context = {
         'campaign': campaign,
         'threads': threads,
         'sessions': sessions,
-        'events': events,
     }
+
+    session = request.GET.get('session')
+    if session:
+        context['selected_session'] = int(session)
+        events = events.filter(session__pk=session)
+
+    thread = request.GET.get('thread')
+    if thread:
+        context['selected_thread'] = int(thread)
+        events = events.filter(threads__pk=thread)
+    context['events'] = events
+
     return render(request, 'main.html', context)
 
