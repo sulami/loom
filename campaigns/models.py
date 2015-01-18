337172bf20818f53914cc46c2685e47da09f8ea2
Robin Schroer <sulami@peerwire.org>
2015-01-18 13:31:06 +0100

    add __str__() functions to all the models

--- campaigns/models.py
+++ campaigns/models.py
@@ -7,17 +7,29 @@ class Campaign(models.Model):
     name = models.CharField(max_length=100)
     owner = models.ForeignKey(AUTH_USER_MODEL)
 
+    def __str__(self):
+        return '{}\'s {}'.format(self.owner, self.name)
+
 class Session(models.Model):
     campaign = models.ForeignKey(Campaign)
     number = models.IntegerField()
 
+    def __str__(self):
+        return '{} - Session {}'.format(self.campaign.__str__(), self.number)
+
 class Thread(models.Model):
     campaign = models.ForeignKey(Campaign)
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return '{} - {}'.format(self.campaign.__str__(), self.name)
 
 class Event(models.Model):
     campaign = models.ForeignKey(Campaign)
     session = models.ForeignKey(Session)
     threads = models.ManyToManyField(Thread)
     content = models.TextField()
+
+    def __str__(self):
+        return '{} - "{}..."'.format(self.campaign.__str__(), self.content[:50])
 
