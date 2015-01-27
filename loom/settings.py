dd5d2935a91d58fcd366b3ed7236811f9ff39281
Robin Schroer <sulami@peerwire.org>
2015-01-27 16:08:04 +0100

    add accounts app

--- loom/settings.py
+++ loom/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django_ajax',
     'ordered_model',
+    'accounts',
     'campaigns',
 )
 
--- loom/urls.py
+++ loom/urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/', include('accounts.urls')),
     url(r'', include(urls, namespace='campaigns')),
 )
 
